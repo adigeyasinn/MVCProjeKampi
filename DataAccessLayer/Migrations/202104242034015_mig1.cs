b3e323356bc3004ecf0657e7b7944c40139865ca
201118063 <201118063@samsun.edu.tr>
2021-04-25 23:40:15 +0300

    Ders 20

---
+++ DataAccessLayer/Migrations/202104242034015_mig1.cs
@@ -0,0 +1,38 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Headings", "Writer_WriterID", "dbo.Writers");
+            DropIndex("dbo.Headings", new[] { "Writer_WriterID" });
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            RenameColumn(table: "dbo.Headings", name: "Writer_WriterID", newName: "WriterID");
+            AlterColumn("dbo.Headings", "WriterID", c => c.Int(nullable: false));
+            AlterColumn("dbo.Contents", "WriterID", c => c.Int());
+            CreateIndex("dbo.Headings", "WriterID");
+            CreateIndex("dbo.Contents", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID");
+            AddForeignKey("dbo.Headings", "WriterID", "dbo.Writers", "WriterID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Headings", "WriterID", "dbo.Writers");
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            DropIndex("dbo.Headings", new[] { "WriterID" });
+            AlterColumn("dbo.Contents", "WriterID", c => c.Int(nullable: false));
+            AlterColumn("dbo.Headings", "WriterID", c => c.Int());
+            RenameColumn(table: "dbo.Headings", name: "WriterID", newName: "Writer_WriterID");
+            CreateIndex("dbo.Contents", "WriterID");
+            CreateIndex("dbo.Headings", "Writer_WriterID");
+            AddForeignKey("dbo.Headings", "Writer_WriterID", "dbo.Writers", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID", cascadeDelete: true);
+        }
+    }
+}
